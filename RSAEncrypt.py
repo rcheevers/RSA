96a7e8a6da0730439402a5c3433b564eaf493e7a
Ryan Cheevers <65794698+rcheevers@users.noreply.github.com>
2020-11-26 01:43:49 -0600

    Update RSAEncrypt.py

--- RSAEncrypt.py
+++ RSAEncrypt.py
@@ -1,17 +1,18 @@
+import json
 
-def encrypt(m):
+def encrypt(m,name="Master"):
     
-    file = open("RSAKeys.txt","r")
-    file.readline()
-    public = file.readline()
+    file = open("RSAKeys.json","r")
+    keyDict = json.load(file)
+    publicD = keyDict[name]["d"]
+    publicN = keyDict[name]["n"]
     file.close()
-    i = 0
-    while i < len(public):
-        if "," == public[i]:
-            commaIndex = i
-            break
-        i = i + 1
-    e = public[12:commaIndex]
-    n = public[commaIndex+2:-1]
-    return pow(m,e,n)
-    
+    return pow(m,publicD,publicN) 
+
+def decrypt(c,name="Master"):
+    file = open("RSAKeys.json","r")
+    keyDict = json.load(file)
+    publicE = keyDict[name]["e"]
+    publicN = keyDict[name]["n"]
+    file.close()
+    return pow(c,publicE,publicN) 
